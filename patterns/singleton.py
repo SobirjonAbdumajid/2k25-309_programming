ac8b72cd5658a41d1fb8bb6d2169a873c7cd75ff
SobirjonAbdumajid <sobirjon0305@gmail.com>
2025-10-17 15:20:54 +0500

    ok

--- patterns/singleton.py
+++ patterns/singleton.py
@@ -1,17 +1,20 @@
-class Singleton:
-    _instance = None
 
-    def __new__(cls):
-        if cls._instance is None:
-            print('Creating the instance')
-            cls._instance = super(Singleton, cls).__new__(cls)
-        return cls._instance
 
-if __name__ == "__main__":
-    s1 = Singleton()
-    s2 = Singleton()
 
-    if id(s1) == id(s2):
-        print("Singleton works, both variables contain the same instance.")
-    else:
-        print("Singleton failed, variables contain different instances.")
+# class Singleton:
+#     _instance = None
+#
+#     def __new__(cls):
+#         if cls._instance is None:
+#             print('Creating the instance')
+#             cls._instance = super(Singleton, cls).__new__(cls)
+#         return cls._instance
+#
+# if __name__ == "__main__":
+#     s1 = Singleton()
+#     s2 = Singleton()
+#
+#     if id(s1) == id(s2):
+#         print("Singleton works, both variables contain the same instance.")
+#     else:
+#         print("Singleton failed, variables contain different instances.")
